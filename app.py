cac58c8d9434e10d447accd1fa35ed9fdabe5c7c
HK <halilkhaled24@gmail.com>
2026-03-10 19:22:44 +0100

    Version 2 - Modification du message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Mon App DevOps 🚀</h1><p>Pipeline Jenkins + Docker</p>"
+    return "<h1>Mon App DevOps </h1><p>Pipeline Jenkins + Docker</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
